dce905dc80c08f427d1ad021389365cb9dde619c
shaqdeff <shaquillendunda@gmail.com>
2022-04-25 10:36:37 +0300

    Add eslint config and fix errors

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const correctAnswers = ['B', 'A', 'C', 'A', 'B', 'B', 'C', 'A', 'B', 'C'];
 const form = document.querySelector('.quiz-form');
 const result = document.querySelector('.result');
 
-form.addEventListener('submit', e => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
   let score = 0;
-  const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value, form.q6.value, form.q7.value, form.q8.value, form.q9.value, form.q10.value];
+  const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value,
+    form.q5.value, form.q6.value, form.q7.value, form.q8.value, form.q9.value, form.q10.value];
 
   // check answers
   userAnswers.forEach((answer, index) => {
@@ -13,8 +14,9 @@ form.addEventListener('submit', e => {
       score += 10;
     }
   });
+
   //  show result on page
-  scrollTo(0, 0);
+  scrollTo(0, 0); // eslint-disable-line
   result.classList.remove('d-none');
 
   // score animation
@@ -24,8 +26,7 @@ form.addEventListener('submit', e => {
     if (output === score) {
       clearInterval(timer);
     } else {
-      output++;
+      output += 1;
     }
   }, 15);
 });
-
