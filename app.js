f2be1687e1a47ea73224eac48925945b57b0a052
shaqdeff <shaquillendunda@gmail.com>
2022-04-25 08:13:54 +0300

    Add event listener to form to check correct answers

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 const correctAnswers = ['B', 'A', 'C', 'A', 'B', 'B', 'C', 'A', 'B', 'C'];
+const form = document.querySelector('.quiz-form');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  let score = 0;
+  const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value, form.q6.value, form.q7.value, form.q8.value, form.q9.value, form.q10.value];
+
+  // check answers
+  userAnswers.forEach((answer, index) => {
+    if (answer === correctAnswers[index]) {
+      score += 10;
+    }
+  });
+});
