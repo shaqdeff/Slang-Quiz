23ebb6f4ead57dedcef1a579b7dd92135dd0072f
shaqdeff <shaquillendunda@gmail.com>
2022-04-25 09:08:31 +0300

    Add scrollTo method to window object

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ form.addEventListener('submit', e => {
     }
   });
   //  show result on page
+  scrollTo(0, 0);
   result.querySelector('span').textContent = `${score}%`;
   result.classList.remove('d-none');
 });
